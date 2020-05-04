724309874b575acf8a3f558c1390fa2dd6d21868
Dominic Della Valle <ddvpublic@gmail.com>
2020-05-03 22:47:49 -0400

    modify readdir test tool's pause order

--- mount/readdir shenanigans.c
+++ mount/readdir shenanigans.c
@@ -3,7 +3,6 @@
 #include <dirent.h>
 
 #define pathBufferSize 1024
-#define stringer(x) #x
 #define markerToStore 2
 
 
@@ -13,7 +12,7 @@ void main(void) {
 	char pathTarg[pathBufferSize] = "";
 	{
 		char *pPtr;
-		if (pPtr = getenv(stringer(TEST_PATH_TARGET))) {
+		if (pPtr = getenv("TEST_PATH_TARGET")) {
 			strncpy(pathTarg, pPtr, pathBufferSize - 1);
 		} else {
 			pathTarg[0] = '.'; // XXX never do this in a real program
@@ -41,19 +40,18 @@ void readdirPath(const char* path) {
 		}
 	}
 
-	seekdir(dir, storedOffset);
-	printf("Readdir again with offset...\n");
+	printf("Seek back to %d and readdir again...\n", storedOffset);
 	getchar();
+	seekdir(dir, storedOffset);
 	while ((ptr = readdir(dir)) != NULL) {
 		offset = telldir(dir);
 		printf("Name:\"%s\"\toffset:%d\n", ptr->d_name, offset);
 	}
 
-
 	printf("rewinddir...");
 	getchar();
 	rewinddir(dir);
-	printf("seekddir...");
+	printf("seekdir to %d...", storedOffset);
 	getchar();
 	seekdir(dir, storedOffset);
 	printf("Readdir again with offset after rewinddir (unspecified behaviour (SUS))...\n");
@@ -62,7 +60,6 @@ void readdirPath(const char* path) {
 		offset = telldir(dir);
 		printf("Name:\"%s\"\toffset:%d\n", ptr->d_name, offset);
 	}
-
 
 	closedir(dir);
 }
